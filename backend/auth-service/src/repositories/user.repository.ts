e19456ec3c34650cf2d504a7953272b8816a285f
1114nikhil <nikhiltk1114@gmail.com>
2025-12-09 05:58:20 +0530

    docker compose complete broken kafka

--- backend/auth-service/src/repositories/user.repository.ts
+++ backend/auth-service/src/repositories/user.repository.ts
@@ -1,17 +1,16 @@
 // src/repositories/user.repository.ts
 import { prisma } from '../lib/prisma';   // ← new import
-import { User } from '../generated/prisma';
-
+import type { UserModel } from '../generated/prisma/models/User';
 export class UserRepository {
-  async createUser(data: { email: string; password: string; name?: string }): Promise<User> {
+  async createUser(data: { email: string; password: string; name?: string }): Promise<UserModel> {
     return prisma.user.create({ data });
   }
 
-  async findByEmail(email: string): Promise<User | null> {
+  async findByEmail(email: string): Promise<UserModel | null> {
     return prisma.user.findUnique({ where: { email } });
   }
 
-  async findById(id: string): Promise<User | null> {
+  async findById(id: string): Promise<UserModel | null> {
     return prisma.user.findUnique({ where: { id } });
   }
 }
